e1321efa96a629e24d8ca86db64ecc5bbbd8a0af
Niko <n1kokara@yandex.ru>
2025-11-12 03:37:00 +0300

    Some site

--- src/Server.hs
+++ src/Server.hs
@@ -2,7 +2,11 @@ module Server where
 
 import           Network.Wai
 import           Network.HTTP.Types
+import           Lucid
+import           Data.Text.Lazy.Encoding (encodeUtf8)
+
+import           HTML.Main
 
 app :: Application
 app _ res =
-  res $ responseLBS status200 [("Content-Type", "text/plain")] "pong"
+  res $ responseLBS status200 [("Content-Type", "text/html; charset=utf-8")] (encodeUtf8 $ renderText bookPage)
