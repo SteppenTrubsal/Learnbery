192d6686d2d023dc6545705f328653407573a9b7
Niko <n1kokara@yandex.ru>
2025-11-24 15:11:39 +0300

    Interim

--- src/Config/Postgre.hs
+++ src/Config/Postgre.hs
@@ -1,13 +1,24 @@
 module Config.Postgre where
 
-import           Control.Lens  (makeLenses)
+import           Control.Lens
 import           Data.Aeson.TH
+import           Data.Maybe (fromMaybe)
+import           Database.PostgreSQL.Simple
 
 data PostgreConf = PostgreConf
   { _host   :: Maybe String
   , _port   :: Maybe Int
   , _user   :: Maybe String
-  , _passwd :: Maybe String 
+  , _passwd :: Maybe String
   }
 makeLenses ''PostgreConf
 $(deriveJSON defaultOptions {fieldLabelModifier = drop 1} ''PostgreConf)
+
+defPostgreConf :: PostgreConf -> ConnectInfo
+defPostgreConf pc =
+  defaultConnectInfo
+    { connectHost = fromMaybe (connectHost defaultConnectInfo) $ pc ^. host
+    , connectPort = maybe (connectPort defaultConnectInfo) fromIntegral (pc ^. port)
+    , connectUser = fromMaybe (connectUser defaultConnectInfo) $ pc ^. user
+    , connectPassword = fromMaybe (connectPassword defaultConnectInfo) $ pc ^. passwd
+    }
