278d1c171f00a985e91ae8b58aaf58d468a9ab5d
Niko <n1kokara@yandex.ru>
2025-11-24 14:52:18 +0300

    Interim

--- src/Config/Pool.hs
+++ src/Config/Pool.hs
@@ -2,18 +2,11 @@ module Config.Pool where
 
 import           Control.Lens  (makeLenses)
 import           Data.Aeson.TH
-import           Data.Default
 
 data PoolConf = PoolConf
-  { _stripes      :: Maybe Int
-  , _keepAlive    :: Maybe Int
-  , _resPerStripe :: Maybe Int
+  { _stripes      :: Int
+  , _keepAlive    :: Int
+  , _resPerStripe :: Int
   }
 makeLenses ''PoolConf
 $(deriveJSON defaultOptions {fieldLabelModifier = drop 1} ''PoolConf)
-
-instance Default PoolConf where
-  def = PoolConf
-    (Just 1)
-    (Just 30)
-    (Just 10)
